cd2757f479dfdf9cf7d8098e23315c65c79cef2a
嘉陽宗智 <tomo.kayo.8982@gmail.com>
2026-04-24 10:29:53 +0000

    service-worker⇒serviceworkerにリネーム

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ root.render(
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js", { scope: "/" });
+    navigator.serviceWorker.register("/serviceworker.js", { scope: "/" });
   });
 }
