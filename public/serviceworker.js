c380c2979686f75908ac3acd0b3c754ff26a50b0
嘉陽宗智 <tomo.kayo.8982@gmail.com>
2026-04-24 11:51:05 +0000

    SWの空のfetchを削除

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,8 +2,6 @@ self.addEventListener("install", () => {
   console.log("Service Worker installed");
 });
 
-self.addEventListener("fetch", () => {});
-
 self.addEventListener("fetch", (event) => {
   // 何もしなくても fetch ハンドラが存在するだけで OK
 });
